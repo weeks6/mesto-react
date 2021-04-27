bdfad36c15e79ac8cbb128241ce332ef6109033a
weeks6 <bridcarp1@gmail.com>
2021-04-28 00:04:07 +0300

    fixed a11y jsx anchor issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,13 @@ export default function Header() {
   return (
     <header className="header container">
       <a
-        href="https://weeks6.github.io/mesto"
+        href="https://weeks6.github.io/mesto-react"
         className="header__logo"
         target="_self"
-      ></a>
+        title='Главная страница Mesto'
+      >
+        <span className="header__logo_type_sr">Главная страница Mesto</span>
+      </a>
     </header>
   )
 }
